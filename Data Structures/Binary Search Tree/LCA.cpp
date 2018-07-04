58e0d6aea8dd0e53d425f347fffb17d7776c024b
Meenakshi <MeenakshiSekhar@users.noreply.github.com>
2018-07-04 09:20:42 +0530

    Update LCA.cpp

--- Data Structures/Binary Search Tree/LCA.cpp
+++ Data Structures/Binary Search Tree/LCA.cpp
@@ -17,18 +17,50 @@ struct node *newNode(int data)
 	return temp;
 }
 
-struct node *lca(struct node *root, int n1, int n2)
+struct node *findLca(struct node *root, int n1, int n2, bool &v1, bool &v2)
 {
 	if(root == NULL)
 		return NULL;
-	
-	if(root->data > n1 && root->data >n2)
-		return lca(root->left, n1, n2);
-	
-	if(root->data > n1 && root->data >n2)
-		return lca(root->right, n1, n2);
 		
-	return root;
+	if(root->data == n1)
+	{
+		v1 = true;
+	}
+	if(root->data == n2)
+	{
+		v2 = true;
+	}
+	
+	if(root->data > n1 && root->data >n2)
+		return findLca(root->left, n1, n2, v1, v2);
+	
+	if(root->data > n1 && root->data >n2)
+		return findLca(root->right, n1, n2, v1, v2);
+		
+	return root;	
+}
+
+bool findNode(struct node *root, int n)
+{
+	if(root == NULL)
+		return false;
+	
+	if(root->data == n || findNode(root->left, n) || findNode(root->right, n))
+		return true;
+	
+	return false;
+}
+
+struct node *lca(struct node *root, int n1, int n2)
+{
+	bool v1 = false;
+	bool v2 = false;
+	struct node *result = findLca(root, n1, n2, v1, v2);
+	if((v1 && v2) || (v1 && findNode(root, n2)) || (v2 && findNode(root, n1)))
+	{
+		return result;
+	}
+	return NULL;
 }
 
 int main()
@@ -41,7 +73,15 @@ int main()
     root->left->right->left  = newNode(10);
     root->left->right->right = newNode(14);
     
-    cout<<lca(root, 4, 12)->data;
+    struct node *result = lca(root, 1, 10);
+    if(result != NULL)
+    {
+    	cout<<result->data<<endl;
+	}
+	else
+	{
+		cout<<"Key not found"<<endl;
+	}
     
     return 0;
 }

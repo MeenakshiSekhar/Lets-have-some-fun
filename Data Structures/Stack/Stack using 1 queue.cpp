359ee03dacc436e670cac9f5fb0a5f6b10f5ec6c
Meenakshi <MeenakshiSekhar@users.noreply.github.com>
2018-07-04 19:05:39 +0530

    Create Stack using 1 queue.cpp

---
+++ Data Structures/Stack/Stack using 1 queue.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Stack
+{
+	queue<int> q;
+	int size;
+	
+	public:
+	Stack()
+	{
+		size = 0; 	
+	}	
+	
+	void push(int n)
+	{
+		size++;
+		q.push(n);
+		
+		int temp = size;
+		while(temp-- > 1)
+		{
+			int front = q.front();
+			q.pop();
+			q.push(front);
+		}
+	}
+	
+	int pop()
+	{
+		if(!q.empty())
+		{
+			int top = q.front();
+			q.pop();
+			return top;
+		}
+	}
+};
+
+int main()
+{
+	Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	s.push(5);
+	
+	cout<<s.pop()<<endl;
+	cout<<s.pop()<<endl;
+	cout<<s.pop()<<endl;
+	
+	return 0;
+	
+}

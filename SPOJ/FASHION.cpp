01878e17ffa8e1d2d366ebf85e66ffead739c02a
Meenakshi <MeenakshiSekhar@users.noreply.github.com>
2018-07-10 22:22:06 +0530

    Create FASHION.cpp

---
+++ SPOJ/FASHION.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long hotness(long long *men, long long *women, int n)
+{
+	long long hotSum = 0;
+	sort(men, men+n);
+	sort(women, women+n);
+	for(int i=0; i<n; i++)
+	{
+		hotSum += (men[i] * women[i]);
+	}
+	return hotSum;
+}
+
+int main()
+{
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		long long men[n];
+		long long women[n];
+		for(int i=0; i<n; i++)
+		{
+			cin>>men[i];
+		}
+		for(int i=0; i<n; i++)
+		{
+			cin>>women[i];
+		}
+		cout<<hotness(men, women, n)<<endl;
+	}
+	return 0;
+}

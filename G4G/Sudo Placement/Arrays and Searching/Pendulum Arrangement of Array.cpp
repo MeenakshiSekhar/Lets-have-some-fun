334538183017bc8e93e1f5371693b9fd16c9b73d
Meenakshi <MeenakshiSekhar@users.noreply.github.com>
2018-07-15 16:54:01 +0530

    check history for another method

--- G4G/Sudo Placement/Arrays and Searching/Pendulum Arrangement of Array.cpp
+++ G4G/Sudo Placement/Arrays and Searching/Pendulum Arrangement of Array.cpp
@@ -15,18 +15,20 @@ int main() {
 	        cin>>arr[i];
 	    }
 	    sort(arr, arr+n);
-        int centre = (n-1)/2;
-        int b[n];
-        int sign = 1;
-        int counter = 1;
-        b[centre] = arr[0];
-        for(int i=1; i<n; i++)
-        {
-            centre += sign*counter;
-            b[centre] = arr[i];
-            sign *= -1;
-            counter++;
-        }
+	    int centre = (n-1)/2;
+	    int b[n];
+	    b[centre] = arr[0];
+	    int j = 1;
+	    int i = 1;
+	    for( i=1; i<=centre; i++)
+	    {
+	    	b[centre+i] = arr[j++];
+	    	b[centre-i] = arr[j++];
+	    }
+	    if(n%2==0)
+	    {
+	    	b[centre+i] = arr[j];
+	    }
 	    for(int i=0; i<n; i++)
 	    {
 	        cout<<b[i]<<" ";

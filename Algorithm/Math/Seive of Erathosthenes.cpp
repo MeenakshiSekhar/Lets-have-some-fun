6dc23f2bb6565c50c14bab32b55479661c0265cc
Meenakshi <MeenakshiSekhar@users.noreply.github.com>
2018-07-30 15:02:33 +0530

    To compute sum of prime numbers

---
+++ Algorithm/Math/Seive of Erathosthenes.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string.h>
+#define ll long long
+using namespace std;
+
+ll primes(ll n)
+{
+	bool arr[n+1];
+	memset(arr, true, n+1);
+	for(ll p = 2; p*p<=n; p++)
+	{
+		if(arr[p] == true)
+		{
+			for(ll x=p*2; x<=n; x+=p)
+			{
+				arr[x] = false;
+			}
+		}
+	}
+	ll sum_of_primes = 0;
+	for(ll i=2; i<=n; i++)
+	{
+		if(arr[i])
+		{
+			sum_of_primes += i;
+		}
+	}
+	return sum_of_primes;
+}
+
+int main()
+{
+	ll n;
+	cin>>n;
+	cout<<primes(n)<<endl;
+	return 0;
+}
+
